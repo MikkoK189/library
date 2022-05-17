d6593410bd0273a0fc8ef4ae00d31eff2a8eaea2
MikkoK189 <mikkok11@gmail.com>
2022-05-17 16:24:29 +0300

    Finished most of the site without committing at all.

---
+++ script.js
@@ -0,0 +1,83 @@
+const card = document.getElementById(0);
+const bookContainer = document.getElementById("books");
+const showButton = document
+  .getElementById("addbook")
+  .addEventListener("click", openForm);
+const hideButton = document
+  .getElementById("closeform")
+  .addEventListener("click", closeForm);
+const form = document.getElementById("myForm");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let elements = event.target.elements;
+  addBookToLibrary(
+    elements["title"].value,
+    elements["author"].value,
+    elements["pages"].value,
+    elements["read"].checked
+  );
+  event.target.reset();
+});
+
+let myLibrary = [];
+
+function Book(title, author, pages, hasRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.hasRead = hasRead;
+}
+
+function addBookToLibrary(title, author, pages, hasRead) {
+  let newBook = new Book(title, author, pages, hasRead);
+  myLibrary.push(newBook);
+  updateBooks();
+}
+
+function removeBook(id) {
+  if (id.target.parentElement.parentElement.classList.contains("card")) {
+    let bookToRemove = id.target.parentElement.parentElement.id;
+    myLibrary.splice(bookToRemove, 1);
+    updateBooks();
+    let elementToRemove = document.getElementById(myLibrary.length);
+    elementToRemove.parentNode.removeChild(elementToRemove);
+  }
+}
+
+function updateBooks() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    if (document.getElementById(i)) {
+      let bookCard = document.getElementById(i);
+      bookCard.id = i;
+      bookCard.querySelector("#title").textContent = myLibrary[i].title;
+      bookCard.querySelector("#author").textContent = myLibrary[i].author;
+      bookCard.querySelector(
+        "#pages"
+      ).textContent = `${myLibrary[i].pages} pages`;
+      bookCard.querySelector("#read").checked = myLibrary[i].hasRead;
+      bookCard.querySelector("#trash").addEventListener("click", removeBook);
+    } else {
+      let bookCard = card.cloneNode(true);
+      bookCard.id = i;
+      bookCard.querySelector("#title").textContent = myLibrary[i].title;
+      bookCard.querySelector("#author").textContent = myLibrary[i].author;
+      bookCard.querySelector(
+        "#pages"
+      ).textContent = `${myLibrary[i].pages} pages`;
+      bookCard.querySelector("#read").checked = myLibrary[i].hasRead;
+      bookCard.querySelector("#trash").addEventListener("click", removeBook);
+      bookContainer.append(bookCard);
+    }
+  }
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+card.parentNode.removeChild(card);
